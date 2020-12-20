ada6baeecd72334d66f6bf43ee86b92f93f1fe84
lyanesga <lyanesga@host.docker.internal>
2020-12-20 09:49:07 +0100

    Incluir jar

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into task(id, task_status, description) values(1, 'In progress', 'Backend, microservicios con Spring boot.'),
+(2, 'Pending', 'Repositorio Proyecto, crear repositorio público de GitHub.'),
+(3, 'Finished', 'DevOps, desplegar imagen de la aplicación desde DockerHub en AKS.');
